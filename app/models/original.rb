d5e4417d5c8b3067e0bd8faf7682a936985b7d3d
akira_iguchi <akituri931@icloud.com>
2022-09-12 00:32:54 +0900

    add: models

--- app/models/original.rb
+++ app/models/original.rb
@@ -1,2 +1,4 @@
 class Original < ApplicationRecord
+  belongs_to :author
+  has_many :secondaries, dependent: :destroy
 end
--- app/models/secondary.rb
+++ app/models/secondary.rb
@@ -1,2 +1,4 @@
 class Secondary < ApplicationRecord
+  belongs_to :author
+  belongs_to :original
 end
