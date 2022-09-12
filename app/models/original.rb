8b79dbba6c7273dce219bc21cc32a77569234eef
akira_iguchi <akituri931@icloud.com>
2022-09-12 16:31:54 +0900

    add: バリデーション追加

--- app/models/original.rb
+++ app/models/original.rb
@@ -1,4 +1,8 @@
 class Original < ApplicationRecord
   belongs_to :author
   has_many :secondaries, dependent: :destroy
+
+  validates :content_id, numericality: { only_integer: true }
+  validates :title, length: { maximum: 30 }, uniqueness: true
+  validates :description, length: { maximum: 255 }
 end
