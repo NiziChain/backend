0a3a6b251758469e2112200cc819502fed10f0dc
akira_iguchi <akituri931@icloud.com>
2022-09-13 00:33:57 +0900

    add: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: "json" } do
-      resource :originals, only: %i(create)
+      resource :originals, only: %i(create show)
       # resource :secondaries, only: %i(create)
       # resource :contents, only: %i(index)
       # resource :authors, only: %i(show)
