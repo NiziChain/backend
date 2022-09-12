6959cd1a5c1ef016c77df7c9d963f3ed3de6b009
akira_iguchi <akituri931@icloud.com>
2022-09-12 16:44:05 +0900

    fix: バリデーション日本語化

--- app/models/original.rb
+++ app/models/original.rb
@@ -2,7 +2,7 @@ class Original < ApplicationRecord
   belongs_to :author
   has_many :secondaries, dependent: :destroy
 
-  validates :content_id, numericality: { only_integer: true }
-  validates :title, length: { maximum: 30 }, uniqueness: true
-  validates :description, length: { maximum: 255 }
+  validates :content_id, presence: true, numericality: { only_integer: true }
+  validates :title, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :description, presence: true, length: { maximum: 255 }
 end
